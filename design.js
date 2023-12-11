0e8c23d65cf44deb2cc4b0e59392286c26eb1ddc
Chand mohammed <153479048+chandmohammed7@users.noreply.github.com>
2023-12-11 12:06:15 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,11 @@
+// for name looping 
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed(".text", {
+    strings: ["Chandu Designer", "Student of Star Infotech"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+  });
+});
+
